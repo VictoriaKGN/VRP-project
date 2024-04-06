0dca58237c7d4ae10e1fc7dac2e16d16655c4e8c
VictoriaKGN <koganv@myumanitoba.ca>
2024-04-06 13:36:19 -0500

    some alg fixes

--- algorithms/two_opt.py
+++ algorithms/two_opt.py
@@ -9,13 +9,13 @@ def two_opt(model, routes, num_iterations):
     total_dist = 0
     num_locations = len(route)
     
-    for i in range(num_locations - 2):
+    for i in range(num_locations - 1):
       curr_location = route[i]
       next_location = route[i + 1]
       total_dist += dist_matrix[curr_location][next_location]
       
     return total_dist
-    
+  
   def two_opt_solver(model, routes, num_iterations):
     distance_matrix = model["distance_matrix"]
     best_routes = routes.copy()
@@ -37,7 +37,7 @@ def two_opt(model, routes, num_iterations):
       if calc_total_distance(new_routes[selected_route_index], distance_matrix) < calc_total_distance(best_routes[selected_route_index], distance_matrix):
         best_routes = new_routes
         
-      return best_routes
+    return best_routes
   
   for i in range(len(routes)):
     route = routes[i]
