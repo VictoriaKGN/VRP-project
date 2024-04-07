0f581d2893a2bb262bf43c28d4d4b687d7749e13
VictoriaKGN <koganv@myumanitoba.ca>
2024-04-07 16:31:45 -0500

    routes and graphs for guided local search and tabu search implemented

--- src/benchmark.py
+++ src/benchmark.py
@@ -6,6 +6,7 @@ from algorithms.guided_local_search import guided_local_search
 from algorithms.tabu_search import tabu_search
 import time
 import numpy as np
+import copy
 
 def random_routes(data):
   """
@@ -87,6 +88,25 @@ def calc_total_distance_ortools(num_vehicles, routing, solution):
   return total
 
 
+def get_routes_ortools(manager, routing, solution, num_vehicles):
+  """
+  Returns routes of OR-Tools algorithms
+  """
+  routes = []
+
+  for vehicle_id in range(num_vehicles):
+    vehicle_route = []
+    index = routing.Start(vehicle_id)
+    while not routing.IsEnd(index):
+      node_index = manager.IndexToNode(index)
+      vehicle_route.append(node_index)
+      index = solution.Value(routing.NextVar(index))
+    vehicle_route.append(manager.IndexToNode(index))
+    routes.append(vehicle_route)
+
+  return routes
+
+
 def test_nearest_neighbour(model):
   """
   Returns routes, total distance and execution time of nearest neighbour algorithm
@@ -113,12 +133,12 @@ def test_random(model):
   return routes, distance, exec_time
 
 
-def test_two_opt(model, routes, iterations):
+def test_two_opt(model, init_routes, iterations):
   """
   Returns routes, total distance and execution time of two-opt algorithm
   """
   start = time.perf_counter_ns()
-  routes = two_opt(model, routes, iterations)
+  routes = two_opt(model, copy.deepcopy(init_routes), iterations)
   end = time.perf_counter_ns()
   exec_time = (end - start) / 1000000000
   distance = calc_total_distance(routes, model['distance_matrix'])
@@ -135,7 +155,9 @@ def test_guided_local_search(model):
   end = time.perf_counter_ns()
   exec_time = (end - start) / 1000000000
   distance = calc_total_distance_ortools(model["num_vehicles"], routing, solution)
-  return distance, exec_time
+  routes = get_routes_ortools(manager, routing, solution, model["num_vehicles"])
+
+  return routes, distance, exec_time
 
 
 def test_tabu_search(model):
@@ -147,8 +169,9 @@ def test_tabu_search(model):
   end = time.perf_counter_ns()
   exec_time = (end - start) / 1000000000
   distance = calc_total_distance_ortools(model["num_vehicles"], routing, solution)
+  routes = get_routes_ortools(manager, routing, solution, model["num_vehicles"])
 
-  return distance, exec_time
+  return routes, distance, exec_time
 
 
 def test_small_map():
@@ -161,15 +184,15 @@ def test_small_map():
   to_routes, to_distance, to_time = test_two_opt(model, nn_routes, 1000)
   rand_routes, rand_distance, rand_time = test_random(model)
   to_rand_routes, to_rand_distance, to_rand_time = test_two_opt(model, rand_routes, 1000)
-  gls_distance, gls_time = test_guided_local_search(model)
-  ts_distance, ts_time = test_tabu_search(model)
+  gls_routes, gls_distance, gls_time = test_guided_local_search(model)
+  ts_routes, ts_distance, ts_time = test_tabu_search(model)
 
   draw_solution(coords, nn_routes, 'nearest neighbour')
   draw_solution(coords,to_routes, 'two-opt (NN)')
-  #draw_solution(coords, ts_routes, 'tabu search')
   draw_solution(coords, rand_routes, 'random solution')
   draw_solution(coords, to_rand_routes, 'two-opt (random input)')
-  #draw_solution(coords, gls_routes, 'guided local search')
+  draw_solution(coords, gls_routes, 'guided local search')
+  draw_solution(coords, ts_routes, 'tabu search')
 
   # temporary print mess, ideally we save to CSV or something?
   print("""
@@ -190,9 +213,11 @@ def test_small_map():
         \tDistance: {torandomdist}
         \tTime: {torandomtime}
         Guided Local Search:
+        \tRoute: {glsroute}
         \tDistance: {glsdistance}
         \tTime: {glstime}
         Tabu Search:
+        \tRoute: {tsroute}
         \tDistance: {tsdistance}
         \tTime: {tstime}
         """.format(
@@ -208,8 +233,10 @@ def test_small_map():
           torandroute=to_rand_routes,
           torandomdist=to_rand_distance,
           torandomtime=to_rand_time,
+          glsroute=gls_routes,
           glsdistance=gls_distance,
           glstime=gls_time,
+          tsroute=ts_routes,
           tsdistance=ts_distance,
           tstime=ts_time
         ))
