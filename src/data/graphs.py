a72e9b92efea70790b9bed40252d49f558a2d219
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2024-04-07 16:18:33 -0500

    highlight the depot on the graph

--- src/data/graphs.py
+++ src/data/graphs.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 
 def draw_solution(coords, routes, title):
   x, y = zip(*coords)
-  plt.scatter(x[:0], y[:0], color = 'red')
-  plt.scatter(x[1:],y[1:], color = 'blue')
+  plt.scatter(x[:1], y[:1], marker = 's', color = 'red', label='depot')
+  plt.scatter(x[1:],y[1:], label='location')
   plt.title(title)
+  plt.legend()
   # routes have location index, the graph location is x[loc_index], y[loc_index]
   # i have several plot calls to make (each route), but it looks like multiple things can be plotted at once
   for route in routes:
