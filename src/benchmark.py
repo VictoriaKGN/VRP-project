54531855dd489c3a6f8a5202af586048b41c43c2
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2024-04-08 20:50:12 -0500

    mega test of vehicle fleets on varying map sizes

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,5 +1,5 @@
 from data.models import create_model
-from data.graphs import draw_solution
+from data.graphs import draw_solution, draw_map
 from algorithms.nearest_neighbor import nearest_neighbor
 from algorithms.two_opt import two_opt
 from algorithms.guided_local_search import guided_local_search
@@ -179,7 +179,62 @@ def test_small_map():
   Test a series of small maps with 10 points and 2 vehicles with the same capacity
 	"""
   model, coords = create_model(50,10,2,[5,10])
+  draw_map(coords, 'small', True)
+  test_all_algorithms(model, coords, 'small_map')
 
+  # KEPT FOR REFERENCE, CAN BE DELETED ANYTIME
+  # # temporary print mess, ideally we save to CSV or something?
+  # print("""
+  #       Nearest Neighbour:
+  #       \tRoute: {nnroute}
+  #       \tDistance: {nndist}
+  #       \tTime: {nntime}
+  #       Two-Opt (NN):
+  #       \tRoute: {toroute}
+  #       \tDistance: {todist}
+  #       \tTime: {totime}
+  #       Random:
+  #       \tRoute: {randroute}
+  #       \tDistance: {randdist}
+  #       \tTime: {randtime}
+  #       Two-Opt (rand):
+  #       \tRoute: {torandroute}
+  #       \tDistance: {torandomdist}
+  #       \tTime: {torandomtime}
+  #       Guided Local Search:
+  #       \tRoute: {glsroute}
+  #       \tDistance: {glsdistance}
+  #       \tTime: {glstime}
+  #       Tabu Search:
+  #       \tRoute: {tsroute}
+  #       \tDistance: {tsdistance}
+  #       \tTime: {tstime}
+  #       """.format(
+  #         nnroute=nn_routes,
+  #         nndist=nn_distance,
+  #         nntime=nn_time,
+  #         toroute=to_routes,
+  #         todist=to_distance,
+  #         totime=to_time,
+  #         randroute=rand_routes,
+  #         randdist=rand_distance,
+  #         randtime=rand_time,
+  #         torandroute=to_rand_routes,
+  #         torandomdist=to_rand_distance,
+  #         torandomtime=to_rand_time,
+  #         glsroute=gls_routes,
+  #         glsdistance=gls_distance,
+  #         glstime=gls_time,
+  #         tsroute=ts_routes,
+  #         tsdistance=ts_distance,
+  #         tstime=ts_time
+  #       ))
+
+
+def test_all_algorithms(model, coords, config_name):
+  """
+  Run each algorithm on the given model and save to PNG with the given config name
+  """
   nn_routes, nn_distance, nn_time = test_nearest_neighbour(model)
   to_routes, to_distance, to_time = test_two_opt(model, nn_routes, 1000)
   rand_routes, rand_distance, rand_time = test_random(model)
@@ -187,59 +242,47 @@ def test_small_map():
   gls_routes, gls_distance, gls_time = test_guided_local_search(model)
   ts_routes, ts_distance, ts_time = test_tabu_search(model)
 
-  draw_solution(coords, nn_routes, 'nearest neighbour')
-  draw_solution(coords,to_routes, 'two-opt (NN)')
-  draw_solution(coords, rand_routes, 'random solution')
-  draw_solution(coords, to_rand_routes, 'two-opt (random input)')
-  draw_solution(coords, gls_routes, 'guided local search')
-  draw_solution(coords, ts_routes, 'tabu search')
+  draw_map(coords, config_name, True)
+  draw_solution(coords, nn_routes, config_name + ' nearest neighbour')
+  draw_solution(coords,to_routes, config_name + ' two-opt (NN)')
+  draw_solution(coords, rand_routes, config_name + ' random solution')
+  draw_solution(coords, to_rand_routes, config_name + ' two-opt (random input)')
+  draw_solution(coords, gls_routes, config_name + ' guided local search')
+  draw_solution(coords, ts_routes, config_name + ' tabu search')
 
-  # temporary print mess, ideally we save to CSV or something?
-  print("""
-        Nearest Neighbour:
-        \tRoute: {nnroute}
-        \tDistance: {nndist}
-        \tTime: {nntime}
-        Two-Opt (NN):
-        \tRoute: {toroute}
-        \tDistance: {todist}
-        \tTime: {totime}
-        Random:
-        \tRoute: {randroute}
-        \tDistance: {randdist}
-        \tTime: {randtime}
-        Two-Opt (rand):
-        \tRoute: {torandroute}
-        \tDistance: {torandomdist}
-        \tTime: {torandomtime}
-        Guided Local Search:
-        \tRoute: {glsroute}
-        \tDistance: {glsdistance}
-        \tTime: {glstime}
-        Tabu Search:
-        \tRoute: {tsroute}
-        \tDistance: {tsdistance}
-        \tTime: {tstime}
-        """.format(
-          nnroute=nn_routes,
-          nndist=nn_distance,
-          nntime=nn_time,
-          toroute=to_routes,
-          todist=to_distance,
-          totime=to_time,
-          randroute=rand_routes,
-          randdist=rand_distance,
-          randtime=rand_time,
-          torandroute=to_rand_routes,
-          torandomdist=to_rand_distance,
-          torandomtime=to_rand_time,
-          glsroute=gls_routes,
-          glsdistance=gls_distance,
-          glstime=gls_time,
-          tsroute=ts_routes,
-          tsdistance=ts_distance,
-          tstime=ts_time
-        ))
+
+def test_fleets(model, coords, fleet_sizes, total_demand):
+  """
+  Test different fleet configurations on the same network of locations and demands
+  """
+  total_capacity = int(total_demand * 1.2) # introduce a buffer to the vehicle capacities
+  for size in fleet_sizes:
+    # test fleet where each vehicle has enough capacity to hit every location by itself
+    model = change_fleet_config(model, size, [total_capacity]*size)
+    test_all_algorithms(model, coords, '{x}V,1C'.format(x=size))
+    
+    # test where each vehicle has capacity = total_capacity/num_vehicles
+    split_capacity = total_capacity//size if total_capacity//size > 0 else 1
+    model = change_fleet_config(model, size, [split_capacity]*size)
+    test_all_algorithms(model, coords, '{x}V,splitC'.format(x=size))
+
+
+def test_fleet_configs_on_maps(fleet_sizes, location_counts):
+  """
+  Runs each algorithm on a set of maps. Tests multiple fleet configurations for each map.
+  Each algorithm runs every combination of fleet size and location count.
+  """
+  total_demand = 1000
+  for i in location_counts:
+    # initially created using 1 vehicle with total_demand capacity, modified in test_fleets
+    model, coords = create_model(1000, i, 1, [total_demand])
+    test_fleets(model, coords, fleet_sizes, i)
+
+
+def change_fleet_config(model, num_vehicles, capacities):
+  model['num_vehicles'] = num_vehicles
+  model['vehicle_capacities'] = capacities
+  return model
 
 
 def benchmark_suite():
@@ -247,9 +290,12 @@ def benchmark_suite():
   Run a series of tests on each algorithm and save the results
   """
   print('starting benchmarks...')
-  test_small_map()
+  # model, coords = create_model(1000, 5, 1, [5])
+  # test_all_algorithms(model, coords, '5L,1V,1C,1000scale')
+  fleet_sizes = [1,2,10,25]
+  location_counts = [10,50,200]
+  test_fleet_configs_on_maps(fleet_sizes, location_counts)
   print('benchmark suite complete.')
-
 
 
 benchmark_suite()
