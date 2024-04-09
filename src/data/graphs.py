0c683549b25f7cc9e481a121312243bd2c288eed
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2024-04-08 20:48:40 -0500

    draw initial maps without routes

--- src/data/graphs.py
+++ src/data/graphs.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 
 def draw_solution(coords, routes, title):
-  x, y = zip(*coords)
-  plt.scatter(x[:1], y[:1], marker = 's', color = 'red', label='depot')
-  plt.scatter(x[1:],y[1:], label='location')
-  plt.title(title)
-  plt.legend()
+  """
+  Draw the map with routes taken by vehicles as determined by a routing algorithm
+  """
+  x,y = draw_map(coords, title, False)
   # routes have location index, the graph location is x[loc_index], y[loc_index]
   # i have several plot calls to make (each route), but it looks like multiple things can be plotted at once
   for route in routes:
@@ -13,3 +12,19 @@ def draw_solution(coords, routes, title):
     route_x, route_y = zip(*route_coords)
     plt.plot(route_x, route_y)
   plt.savefig('./data/figures/' + title + '.png')
+  plt.close()
+
+
+def draw_map(coords, title, save):
+  """
+  Draw the empty map showing the depot and locations. Saves to png if save parameter is True
+  """
+  x, y = zip(*coords)
+  plt.scatter(x[:1], y[:1], marker = 's', color = 'red', label='depot')
+  plt.scatter(x[1:],y[1:], label='location')
+  plt.title(title)
+  plt.legend()
+  if save:
+    plt.savefig('./data/figures/' + title + '_map.png')
+    plt.close()
+  return x, y
